012ea43b2cb48b022167de8c05a69ecea562dc19
Roman Korostinskiy <70313618+c71n93@users.noreply.github.com>
2024-09-14 13:31:40 +0300

    implement operators

--- calc/src/lib.rs
+++ calc/src/lib.rs
@@ -1,4 +1,5 @@
 use std::fmt::{Display, Formatter};
+use std::ops::{Add, Div, Mul, Sub};
 use crate::calc_errors::CalculationError;
 
 pub mod calc_errors {
@@ -13,6 +14,13 @@ pub struct Calc {
 }
 
 impl Calc {
+
+    pub fn new(x: i32) -> Self {
+        Self {
+            reg: Ok(x)
+        }
+    }
+
     // TODO: To fix copy-paste and lots of boilerplate code in every method.
     pub fn add(mut self, rhs: i32) -> Self {
         match self.reg {
@@ -22,6 +30,18 @@ impl Calc {
             Err(e) => self.reg = Err(e)
         }
         self
+    }
+
+    fn add_self(self, rhs: Self) -> Self {
+        match rhs.reg {
+            Ok(x) => self.add(x),
+            Err(rhs_err) => Self {
+                reg: match self.reg {
+                    Ok(_) => Err(rhs_err),
+                    Err(self_err) => Err(self_err)
+                }
+            }
+        }
     }
 
     pub fn sub(mut self, rhs: i32) -> Self {
@@ -34,6 +54,18 @@ impl Calc {
         self
     }
 
+    fn sub_self(self, rhs: Self) -> Self {
+        match rhs.reg {
+            Ok(x) => self.sub(x),
+            Err(rhs_err) => Self {
+                reg: match self.reg {
+                    Ok(_) => Err(rhs_err),
+                    Err(self_err) => Err(self_err)
+                }
+            }
+        }
+    }
+
     pub fn mul(mut self, rhs: i32) -> Self {
         match self.reg {
             Ok(x) => self.reg = x.checked_mul(rhs).ok_or(
@@ -42,6 +74,18 @@ impl Calc {
             Err(e) => self.reg = Err(e)
         }
         self
+    }
+
+    fn mul_self(self, rhs: Self) -> Self {
+        match rhs.reg {
+            Ok(x) => self.mul(x),
+            Err(rhs_err) => Self {
+                reg: match self.reg {
+                    Ok(_) => Err(rhs_err),
+                    Err(self_err) => Err(self_err)
+                }
+            }
+        }
     }
 
     pub fn div(mut self, rhs: i32) -> Self {
@@ -58,6 +102,18 @@ impl Calc {
             Err(e) => self.reg = Err(e)
         }
         self
+    }
+
+    fn div_self(self, rhs: Self) -> Self {
+        match rhs.reg {
+            Ok(x) => self.div(x),
+            Err(rhs_err) => Self {
+                reg: match self.reg {
+                    Ok(_) => Err(rhs_err),
+                    Err(self_err) => Err(self_err)
+                }
+            }
+        }
     }
 }
 
@@ -87,6 +143,38 @@ impl Default for Calc {
     }
 }
 
+impl Add for Calc {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        self.add_self(rhs)
+    }
+}
+
+impl Sub for Calc {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        self.sub_self(rhs)
+    }
+}
+
+impl Mul for Calc {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        self.mul_self(rhs)
+    }
+}
+
+impl Div for Calc {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self {
+        self.div_self(rhs)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::Calc;
@@ -107,6 +195,31 @@ mod test {
     fn division_by_zero() {
         let res = Calc::default().div(0);
         assert_eq!("Error! Division by zero. Occurred in 'div'", res.to_string());
+    }
+
+    #[test]
+    fn merge_error_first() {
+        let c1 = Calc::default().add(i32::MAX).mul(2);
+        let c2 = c1.sub_self(Calc::default().div(0));
+        assert_eq!("Error! Integer overflow. Occurred in 'mul'", c2.to_string())
+    }
+
+    #[test]
+    fn merge_error_second() {
+        let c1 = Calc::default().add(1);
+        let c2 = c1.mul_self(Calc::default().div(0));
+        assert_eq!("Error! Division by zero. Occurred in 'div'", c2.to_string())
+    }
+
+    #[test]
+    fn operators() {
+        let a = 2;
+        let b = 3;
+        let c = 4;
+        let d = 2;
+        let actual = (a + b) * c / d;
+        let res = (Calc::new(a) + Calc::new(b)) * Calc::new(c) / Calc::new(d);
+        assert_eq!(format!("Result: {actual}"), res.to_string())
     }
 }
 
