4ab73b1eba3863dd149dce78ace3ceabda353a2d
Roman Korostinskiy <70313618+c71n93@users.noreply.github.com>
2024-09-21 18:45:47 +0300

    refactor calc

--- calc/src/lib.rs
+++ calc/src/lib.rs
@@ -1,6 +1,6 @@
+use crate::calc_errors::CalculationError;
 use std::fmt::{Display, Formatter};
 use std::ops::{Add, Div, Mul, Sub};
-use crate::calc_errors::CalculationError;
 
 pub mod calc_errors {
     pub enum CalculationError {
@@ -10,109 +10,67 @@ pub mod calc_errors {
 }
 
 pub struct Calc {
-    reg: Result<i32, CalculationError>
+    reg: Result<i32, CalculationError>,
 }
 
 impl Calc {
-
     pub fn new(x: i32) -> Self {
-        Self {
-            reg: Ok(x)
-        }
+        Self { reg: Ok(x) }
     }
 
-    // TODO: To fix copy-paste and lots of boilerplate code in every method.
     pub fn add(mut self, rhs: i32) -> Self {
-        match self.reg {
-            Ok(x) => self.reg = x.checked_add(rhs).ok_or(
-                CalculationError::IntegerOverflow(String::from("Occurred in 'add'"))
-            ),
-            Err(e) => self.reg = Err(e)
-        }
+        self.reg = self.reg.and_then(|reg| {
+            reg.checked_add(rhs)
+                .ok_or(CalculationError::IntegerOverflow(String::from(
+                    "Occurred in 'add'",
+                )))
+        });
         self
-    }
-
-    fn add_self(self, rhs: Self) -> Self {
-        match rhs.reg {
-            Ok(x) => self.add(x),
-            Err(rhs_err) => Self {
-                reg: match self.reg {
-                    Ok(_) => Err(rhs_err),
-                    Err(self_err) => Err(self_err)
-                }
-            }
-        }
     }
 
     pub fn sub(mut self, rhs: i32) -> Self {
-        match self.reg {
-            Ok(x) => self.reg = x.checked_sub(rhs).ok_or(
-                CalculationError::IntegerOverflow(String::from("Occurred in 'sub'"))
-            ),
-            Err(e) => self.reg = Err(e)
-        }
+        self.reg = self.reg.and_then(|reg| {
+            reg.checked_sub(rhs)
+                .ok_or(CalculationError::IntegerOverflow(String::from(
+                    "Occurred in 'sub'",
+                )))
+        });
         self
-    }
-
-    fn sub_self(self, rhs: Self) -> Self {
-        match rhs.reg {
-            Ok(x) => self.sub(x),
-            Err(rhs_err) => Self {
-                reg: match self.reg {
-                    Ok(_) => Err(rhs_err),
-                    Err(self_err) => Err(self_err)
-                }
-            }
-        }
     }
 
     pub fn mul(mut self, rhs: i32) -> Self {
-        match self.reg {
-            Ok(x) => self.reg = x.checked_mul(rhs).ok_or(
-                CalculationError::IntegerOverflow(String::from("Occurred in 'mul'"))
-            ),
-            Err(e) => self.reg = Err(e)
-        }
+        self.reg = self.reg.and_then(|reg| {
+            reg.checked_mul(rhs)
+                .ok_or(CalculationError::IntegerOverflow(String::from(
+                    "Occurred in 'mul'",
+                )))
+        });
         self
-    }
-
-    fn mul_self(self, rhs: Self) -> Self {
-        match rhs.reg {
-            Ok(x) => self.mul(x),
-            Err(rhs_err) => Self {
-                reg: match self.reg {
-                    Ok(_) => Err(rhs_err),
-                    Err(self_err) => Err(self_err)
-                }
-            }
-        }
     }
 
     pub fn div(mut self, rhs: i32) -> Self {
-        match self.reg {
-            Ok(x) => self.reg = x.checked_div(rhs).ok_or_else( || {
-                    let msg = "Occurred in 'div'";
-                    if rhs == 0 {
-                        CalculationError::DivisionByZero(String::from(msg))
-                    } else {
-                        CalculationError::IntegerOverflow(String::from(msg))
-                    }
+        self.reg = self.reg.and_then(|reg| {
+            reg.checked_div(rhs).ok_or_else(|| {
+                let msg = "Occurred in 'div'";
+                if rhs == 0 {
+                    CalculationError::DivisionByZero(String::from(msg))
+                } else {
+                    CalculationError::IntegerOverflow(String::from(msg))
                 }
-            ),
-            Err(e) => self.reg = Err(e)
-        }
+            })
+        });
         self
     }
 
-    fn div_self(self, rhs: Self) -> Self {
+    fn self_op(self, rhs: Self, op: fn(Self, i32) -> Self) -> Self {
         match rhs.reg {
-            Ok(x) => self.div(x),
+            Ok(x) => op(self, x),
             Err(rhs_err) => Self {
                 reg: match self.reg {
                     Ok(_) => Err(rhs_err),
-                    Err(self_err) => Err(self_err)
-                }
-            }
+                    Err(self_err) => Err(self_err),
+                },
+            },
         }
     }
 }
@@ -122,24 +80,20 @@ impl Display for Calc {
         match &self.reg {
             Ok(r) => write!(f, "Result: {r}"),
             Err(e) => match e {
-                CalculationError::IntegerOverflow(msg) => write!(
-                    f,
-                    "Error! Integer overflow. {msg}"
-                ),
-                CalculationError::DivisionByZero(msg) => write!(
-                    f,
-                    "Error! Division by zero. {msg}"
-                )
-            }
+                CalculationError::IntegerOverflow(msg) => {
+                    write!(f, "Error! Integer overflow. {msg}")
+                }
+                CalculationError::DivisionByZero(msg) => {
+                    write!(f, "Error! Division by zero. {msg}")
+                }
+            },
         }
     }
 }
 
 impl Default for Calc {
     fn default() -> Self {
-        Self {
-            reg: Ok(0)
-        }
+        Self { reg: Ok(0) }
     }
 }
 
@@ -147,7 +101,7 @@ impl Add for Calc {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self {
-        self.add_self(rhs)
+        self.self_op(rhs, Calc::add)
     }
 }
 
@@ -155,7 +109,7 @@ impl Sub for Calc {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self {
-        self.sub_self(rhs)
+        self.self_op(rhs, Calc::sub)
     }
 }
 
@@ -163,7 +117,7 @@ impl Mul for Calc {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self {
-        self.mul_self(rhs)
+        self.self_op(rhs, Calc::mul)
     }
 }
 
@@ -171,7 +125,7 @@ impl Div for Calc {
     type Output = Self;
 
     fn div(self, rhs: Self) -> Self {
-        self.div_self(rhs)
+        self.self_op(rhs, Calc::div)
     }
 }
 
@@ -188,26 +142,32 @@ mod test {
     #[test]
     fn overflow() {
         let res = Calc::default().add(i32::MAX).add(1);
-        assert_eq!("Error! Integer overflow. Occurred in 'add'", res.to_string());
+        assert_eq!(
+            "Error! Integer overflow. Occurred in 'add'",
+            res.to_string()
+        );
     }
 
     #[test]
     fn division_by_zero() {
         let res = Calc::default().div(0);
-        assert_eq!("Error! Division by zero. Occurred in 'div'", res.to_string());
+        assert_eq!(
+            "Error! Division by zero. Occurred in 'div'",
+            res.to_string()
+        );
     }
 
     #[test]
     fn merge_error_first() {
         let c1 = Calc::default().add(i32::MAX).mul(2);
-        let c2 = c1.sub_self(Calc::default().div(0));
+        let c2 = c1 - Calc::default().div(0);
         assert_eq!("Error! Integer overflow. Occurred in 'mul'", c2.to_string())
     }
 
     #[test]
     fn merge_error_second() {
         let c1 = Calc::default().add(1);
-        let c2 = c1.mul_self(Calc::default().div(0));
+        let c2 = c1 * Calc::default().div(0);
         assert_eq!("Error! Division by zero. Occurred in 'div'", c2.to_string())
     }
 
@@ -222,4 +182,3 @@ mod test {
         assert_eq!(format!("Result: {actual}"), res.to_string())
     }
 }
-
